64dbf1a118a6a6d867c1fbb447f8338341399d96
worldberlin <worldberlin60@gmail.com>
2026-04-26 18:09:28 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://washer-dryer-repair-tx.netlify.app/',
+  output: 'static'
 });
